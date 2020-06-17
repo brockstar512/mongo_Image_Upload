4aa8c9a7cdefb5f1e0824fc23afab92f4060a1ff
marshall brock <marshallbrock@gmail.com>
2020-06-16 22:49:16 -0500

    controllers working

--- server.js
+++ server.js
@@ -66,6 +66,42 @@ res.render('index')
 });
 
 //post = upload
+                  //upload is the storage variable and 
+                 //the word single is the amount of files you want ot uplaod at once
+                 //input name ="file" that name is what you are referencing here
+app.post('/upload', upload.single('file'), (req, res)=> {
+// res.json({file:req.file}); // this will respond with the json info
+res.redirect('/')
+})
+//more routes
+//get req
+//display all files
+app.get('/files', (req, res)=>{
+gfs.files.find().toArray((err,files)=>{
+    if(!files || files.length === 0){
+        return res.status(404).json({
+            err: 'no files exist'
+        })
+    }
+    return res.json(files);
+ })
+})
+//get file by name
+app.get('/files/:filename', (req, res)=>{
+    gfs.files.findOne({filename: req.params.filename}, (err, file)=>{
+        if(!file || file.length === 0){
+            return res.status(404).json({
+                err: 'no file exist'
+            })
+        }
+        //file exists
+        return res.json(file)
+    })
+    })
+
+
+//in order to acually interprest the json data as an image we have to use read stream
+
 
 const port = 5000
 
@@ -75,3 +111,21 @@ app.listen(port, () => console.log(`server started on ${port}`))
 // Multer is a NodeJS middleware which facilitates file uploads. 
 //And GridFsStorage is GridFS storage engine for 
 //Multer to store uploaded files directly to MongoDB.
+
+
+//heres the response on upload
+// "file": {
+//     "fieldname": "file",
+//     "originalname": "Screen Shot 2020-05-22 at 10.28.56 PM.png",
+//     "encoding": "7bit",
+//     "mimetype": "image/png",
+//     "id": "5ee986dd4032b72276bf6e6f",
+//     "filename": "cc4d03e9badb2f586a91c5b10dda1a0e.png", (this was generated through the crypto thing.)
+//     "metadata": null,
+//     "bucketName": "uploads",
+//     "chunkSize": 261120,
+//     "size": 164578,
+//     "md5": "05340f62e1f43d34abc15383035623f5",
+//     "uploadDate": "2020-06-17T02:58:37.203Z",
+//     "contentType": "image/png"
+//     }
